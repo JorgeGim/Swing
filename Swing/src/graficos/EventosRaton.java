7d667fac9497c7315e9ef8ed4944d83ae433a75b
jorge <jorge@jorge-SATELLITE-L845>
2017-12-29 15:43:54 -0300

    Eventos de raton 2da parte

--- Swing/src/graficos/EventosRaton.java
+++ Swing/src/graficos/EventosRaton.java
@@ -2,6 +2,7 @@ package graficos;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 
@@ -22,21 +23,33 @@ class MarcoRaton extends JFrame{
 		setBounds(700, 300, 600, 450);
 		EventosDeRaton eventosDeRaton = new EventosDeRaton();
 		addMouseListener(eventosDeRaton);
+		addMouseMotionListener(eventosDeRaton);
 	}
 }
 
-class EventosDeRaton implements MouseListener{
+class EventosDeRaton implements MouseListener, MouseMotionListener{
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		
-		System.out.println("Hiciste click!");
+		//System.out.println("Coordenada X:" + e.getX() + " Coordenada Y: " + e.getY());
+		System.out.println(e.getClickCount()); //cantidad de clicks
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		
-		System.out.println("Acabas de presionar");
+		//System.out.println(e.getModifiersEx()); //devuelve valores de las constantes
+		if(e.getModifiersEx() == 1024) {
+			
+			System.out.println("Presionaste el boton izquierdo");
+		}else if(e.getModifiersEx() == MouseEvent.BUTTON2_DOWN_MASK) {
+			
+			System.out.println("Presionaste la rueda");
+		}else if(e.getModifiersEx() == MouseEvent.BUTTON3_DOWN_MASK) {
+			
+			System.out.println("Presionaste el boton derecho");
+		}
 	}
 
 	@Override
@@ -55,5 +68,17 @@ class EventosDeRaton implements MouseListener{
 	public void mouseExited(MouseEvent e) {
 		
 		System.out.println("Acabas de salir");
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		
+		System.out.println("Estas arrastrando!");
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+		System.out.println("Estas moviendo!");
 	}
 }
